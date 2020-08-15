4f8bcab24732ea4f0cece3c9d8cb15fa060caaeb
okps123 <okps123@gmail.com>
2020-08-15 21:57:47 +0900

    add(web): PurgeCSS 설정

--- web/preact.config.js
+++ web/preact.config.js
@@ -1,6 +1,12 @@
 const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = (config, env, helpers) => {
+  purgecss({
+    content: ["./src/**/*.js"],
+    defaultExtractor: (content) => content.match(params.regex) || [],
+  });
+
   const postCssLoaders = helpers.getLoadersByName(config, "postcss-loader");
   postCssLoaders.forEach(({ loader }) => {
     const { plugins } = loader.options;
